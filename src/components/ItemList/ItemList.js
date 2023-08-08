6efe61ea7ac937689959686b140c632afdc160e5
Jonas <jonathan123.martinez@gmail.com>
2023-08-07 20:45:47 -0400

    Arreglando error con la key

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -2,7 +2,7 @@ import Item from '../Item/Item'
 
 const ItemList = ({products}) => {
     return (
-        <div className="is-flex">
+        <div className="is-flex" key={products.id}>
             {products.map(prod =><Item key={prod.id} {...prod}/>)}
         </div>
     )
